91582bdb81bfa776193a39e947ea717fca62e282
Jonathan Oh <jonathanohs@gmail.com>
2020-09-19 16:13:55 -0700

    first commit

---
+++ DayTrader/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DayTrader
+//
+//  Created by Jonathan Oh on 9/19/20.
+//  Copyright © 2020 Jonathan Oh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
