aff479b8f5af4d91b00b573e766db6c12ca420bb
Jonathan Pun <jpun@jonathans-mbp.gateway.sonic.net>
2020-09-22 14:56:09 -0700

    sceond commit

--- DayTrader/ViewController.swift
+++ DayTrader/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .yellow
+        view.backgroundColor = .blue
     }
 
 
